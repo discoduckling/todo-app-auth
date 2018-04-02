37879002f21a348d49d655eb50ae6747a282d17d
Mitalee Desai <mitalee.n.desai@gmail.com>
2018-04-02 17:07:40 -0600

    Add auth0 strategy

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const express = require('express');
+const passport = require('passport');
+const Auth0Strategy = require('passport-auth0');
+
 app = express();
-app.get('/', (req, res) => {
-    res.send({hi: 'there'});
-})
+
+const auth0Strategy = new Auth0Strategy({
+    domain: 'https://todo-app-discoduckling.auth0.com',
+    clientID: 'vtVB02OQFeazAgpLDx_3TbgOHe8pzVAS',
+    clientSecret: 'E88a-S2nNAQ5ZMV-B_K7DfTfx3QZKBFuVZYB1kg9YkQH1VNZcC-ZcIK9yHlaYAGb',
+    callbackURL: '/auth/auth0/callback'
+});
+
+passport.use(auth0Strategy);
+
+app.get('/auth/auth0/callback', passport.authenticate('auth0', (req, res) => {
+    res.send('login successful');
+}));
+
+// app.get('/', (req, res) => {
+//     res.send({hi: 'there'});
+// })
 
 const PORT = process.env.PORT || 5000;
 
