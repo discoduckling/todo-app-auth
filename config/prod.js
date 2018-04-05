4fe5dcde57000d6d5c720b5cacfe5dfb81fcbbb7
discoduckling <mitalee.n.desai@gmail.com>
2018-04-05 17:36:10 -0600

    Add COOKIE_KEY variable to prod keys

--- config/prod.js
+++ config/prod.js
@@ -3,5 +3,6 @@ module.exports = {
     CLIENT_ID_AUTH0: process.env.CLIENT_ID_AUTH0,
     CLIENT_SECRET_AUTH0: process.env.CLIENT_SECRET_AUTH0,
     AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
-    CALLBACK_URL: process.env.CALLBACK_URL
+    CALLBACK_URL: process.env.CALLBACK_URL,
+    COOKIE_KEY: process.env.COOKIE_KEY
 }
